d54bf852d087acbfe790636f95b742f3ae10fc9a
Ivan Kuznetsov <kis8P@yandex.ru>
2014-05-06 14:25:32 +0000

    Clean up

--- tests/testrunner/test_elliptics.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="module", params=pytest.testrunner.tests.keys())
-def test_name(request):
-    return request.param
-
-def test_elliptics(test_name):
-    pytest.testrunner.run(test_name)
-
