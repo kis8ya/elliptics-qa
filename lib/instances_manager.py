21b01bb8d35c2db4a6e90b2be38861518d8020b1
Ivan Kuznetsov <kis8P@yandex.ru>
2014-06-06 16:35:01 +0000

    hotfix: for rebuild bug

--- lib/instances_manager.py
+++ lib/instances_manager.py
@@ -29,7 +29,8 @@ def create(instances_cfg):
         instances_names = openstack.utils.get_instances_names_from_conf(instance_cfg)
         instances += instances_names
         for instance_name in instances_names:
-            if _satisfied(instance_name, instance_cfg["flavor_name"]):
+            #TODO: temporary fix for rebuil bug
+            if False and _satisfied(instance_name, instance_cfg["flavor_name"]):
                 session.rebuild_instance(instance_name)
             else:
                 icfg = copy.deepcopy(instance_cfg)
