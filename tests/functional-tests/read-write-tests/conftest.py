84886d2e93a338cc833127ec8d2a987d1feed1ce
Ivan Kuznetsov <kis8P@yandex.ru>
2014-06-10 14:57:23 +0000

    tests: elliptics timeouts customization was removed from read-write-tests

--- tests/functional-tests/read-write-tests/conftest.py
+++ tests/functional-tests/read-write-tests/conftest.py
@@ -2,6 +2,4 @@
 #
 #TODO: replace Exception with a proper exception class from elliptics module
 def pytest_addoption(parser):
-    parser.addoption('--wait_timeout', type='int')
-    parser.addoption('--check_timeout', type='int')
-    parser.addoption('--node', type='string', action='append')
+    parser.addoption('--node', type='string', action='append', dest="nodes")
