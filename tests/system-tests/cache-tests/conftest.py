054e9f505314a0e61c300e5d08498eec94f1b2d6
Ivan Kuznetsov <kis8P@yandex.ru>
2014-06-18 14:31:21 +0000

    tests: unused parameter was removed (cache tests)

--- tests/system-tests/cache-tests/conftest.py
+++ tests/system-tests/cache-tests/conftest.py
@@ -1,4 +1,3 @@
 def pytest_addoption(parser):
     parser.addoption('--node', type='string', action='append', dest="nodes")
-    parser.addoption('--show-progress', action='store_true', dest="show_progress")
     parser.addoption('--requests-number', type='int', dest="requests_number")
