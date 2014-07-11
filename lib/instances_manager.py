fdd16c417380c8a8105d2735d58249ebebe3aa8a
Ivan Kuznetsov <kis8P@yandex.ru>
2014-07-11 13:51:01 +0000

    fix: "Instance ... already exists" problem

--- lib/instances_manager.py
+++ lib/instances_manager.py
@@ -39,6 +39,15 @@ def create(instances_cfg):
                 icfg = {"servers": [icfg]}
 
                 session.delete_instance(instance_name)
+                # Wait for instance deletion
+                for i in xrange(120):
+                    try:
+                        info = session.get_instance_info(instance_name)
+                        if info is None:
+                            break
+                        time.sleep(1)
+                    except openstack.utils.ApiRequestError:
+                        break
                 session.create_instances(icfg, check=False)
 
     return openstack.utils.check_availability(session, instances)
