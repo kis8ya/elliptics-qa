5abf36ee435ee8e1c70152e48e9c842c6960429c
Ivan Kuznetsov <kis8P@yandex.ru>
2014-07-18 15:13:57 +0000

    fix: missed file was added

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+import os.path
+import elliptics
+
+from test_helper.elliptics_testhelper import nodes
+
+
+def pytest_addoption(parser):
+    parser.addoption('--node', type='string', action='append', dest="nodes",
+                     help="Elliptics node. Example: --node=hostname:port:group")
+    parser.addoption('--wait-timeout', type='int', dest="wait_timeout",
+                     help="Elliptics wait_timeout.")
+    parser.addoption('--check-timeout', type='int', dest="check_timeout",
+                     help="Elliptics check_timeout.")
+    parser.addoption('--logging-level', type='int', dest="logging_level", default=4,
+                     help="Elliptics logging_level.")
+
+
+@pytest.fixture(scope='module')
+def session(pytestconfig, nodes):
+    """Returns prepared elliptics.Session."""
+    if pytestconfig.option.logging_level:
+        log_path = "/var/log/elliptics/client.log"
+        dir_path = os.path.dirname(log_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        elog = elliptics.Logger(log_path, pytestconfig.option.logging_level)
+    else:
+        elog = elliptics.Logger("/dev/stderr", pytestconfig.option.logging_level)
+
+    config = elliptics.Config()
+    if pytestconfig.option.wait_timeout:
+        config.wait_timeout = pytestconfig.option.wait_timeout
+    if pytestconfig.option.check_timeout:
+        config.check_timeout = pytestconfig.option.check_timeout
+
+    client_node = elliptics.Node(elog, config)
+    for node in nodes:
+        client_node.add_remote(node.host, node.port)
+
+    elliptics_session = elliptics.Session(client_node)
+
+    # Get uniq groups from nodes list
+    groups = list({n.group for n in nodes})
+    elliptics_session.groups = groups
+
+    return elliptics_session
